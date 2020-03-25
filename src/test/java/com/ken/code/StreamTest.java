89e0945de56a788529d309fe4a6cda39b9bf9cf4
wangxiang <xiang.wang@igen-tech.com>
2020-03-25 16:34:32 +0800

    stream部分方法新增

--- src/test/java/com/ken/code/StreamTest.java
+++ src/test/java/com/ken/code/StreamTest.java
@@ -5,7 +5,6 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.Serializable;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
@@ -14,147 +13,205 @@ import java.util.stream.Collectors;
 
 public class StreamTest {
 
-    private List<User> userList;
+	private List<User> userList;
 
-    @Before
-    public void before() {
-        userList = new ArrayList<User>();
-        userList.add(User.builder().code("a").name("aName").age(20).roleId(1).build());
-        userList.add(User.builder().code("c").name("cName").age(10).roleId(1).build());
-        userList.add(User.builder().code("b").name("bName").age(15).roleId(2).build());
-    }
+	@Before
+	public void before() {
+		userList = new ArrayList<User>();
+		userList.add(User.builder().code("a").name("aName").age(20).roleId(1).build());
+		userList.add(User.builder().code("c").name("cName").age(25).roleId(1).build());
+		userList.add(User.builder().code("b").name("bName").age(15).roleId(2).build());
+	}
 
-    @Test
-    public void testAllBlue() {
+	@Test
+	public void test() {
 
-        // 添加重复内容
-        userList.add(User.builder().code("b").name("bName").age(15).roleId(2).build());
+		TreeMap<Integer, Long> collect = userList.stream().collect(Collectors.groupingBy(User::getRoleId, TreeMap::new, Collectors.counting()));
+		System.out.println(collect);
 
-        // 去重
-        List<User> distList = userList.stream().distinct().collect(Collectors.toList());
-        Assert.assertEquals(3, distList.size());
+		TreeMap<Integer, Integer> collect2 = userList.stream().collect(Collectors.groupingBy(User::getRoleId, TreeMap::new, Collectors.reducing(0, item -> item.getAge(), (u, ia) -> u + ia)));
+		System.out.println(collect2);
 
-        // 排序 空内容处理
-        List<User> sortAndLimitList = distList.stream().sorted(Comparator.nullsFirst(Comparator.comparing(User::getAge, this::compareInteger)).reversed()).collect(Collectors.toList());
-        sortAndLimitList.forEach(System.out::println);
+		TreeMap<Integer, User> result = userList.stream().collect(Collectors.groupingBy(User::getRoleId, TreeMap::new, Collectors.reducing(User.builder().age(0).build(), item -> item, (u, item) -> doSomething(u, item))));
+		System.out.println(result);
 
-        // TO Map, Set, List (Collectors.toSet Or toList)
-        Map<String, User> userMap = distList.stream().collect(Collectors.toMap(User::getCode, u -> u));
-        List<String> codeList = distList.stream().map(User::getCode).collect(Collectors.toList());
-        userMap.forEach((k, v) -> System.out.println(k + ":" + v));
-        // To Map fix Duplicate key
-        Map<String, User> userMapDistinct = userList.stream().collect(Collectors.toMap(u -> u.getCode(), u -> u, (v1, v2) -> v2));
-        userMapDistinct.forEach((k, v) -> System.out.println(k + ":" + v));
+//		User reduce = userList.stream().reduce(User.builder().age(0).build(), (u, item) -> doSomething(u, item));
+//		System.out.println(reduce);
+	}
 
+	private User doSomething(User u, User item) {
+		// u参数应该为统计对象，这里不想多定义了，在这里复用
+		// 各种计算 求和，求平均等等
+//		System.out.println("item:" + item);
+//		System.out.println("u:" + u);
+		u.setAge(u.getAge() + item.getAge());
+		return User.builder().age(u.getAge() + item.getAge()).build();
+//		return u;
+	}
 
-        // Statistics: sum max age  and so on
-        int sumAge1 = distList.stream().collect(Collectors.reducing(0, User::getAge, (i, j) -> i + j));
-        int sumAge2 = distList.stream().collect(Collectors.reducing(0, User::getAge, Integer::sum));
-        int sumAge3 = distList.stream().mapToInt(User::getAge).sum();
-        int sumAge4 = distList.stream().collect(Collectors.summingInt(User::getAge));
-        Assert.assertEquals(45, sumAge1);
-        Assert.assertEquals(45, sumAge2);
-        Assert.assertEquals(45, sumAge3);
-        Assert.assertEquals(45, sumAge4);
+	@Test
+	public void testAllBlue() {
 
-        Optional<User> maxAgeUser = distList.stream().max(Comparator.comparingInt(User::getAge));
-        Assert.assertEquals("a", maxAgeUser.get().getCode());
+		// 添加重复内容
+		userList.add(User.builder().code("b").name("bName").age(15).roleId(2).build());
 
-        int maxAge1 = distList.stream().collect(Collectors.reducing(0, User::getAge, Integer::max));
-        int maxAge2 = distList.stream().mapToInt(User::getAge).max().orElse(-1);
-        Assert.assertEquals(20, maxAge1);
-        Assert.assertEquals(20, maxAge2);
+		// 去重
+		List<User> distList = userList.stream().distinct().collect(Collectors.toList());
+		Assert.assertEquals(3, distList.size());
 
-        Double ageAge = distList.stream().collect(Collectors.averagingInt(User::getAge));
-        Assert.assertEquals(15, ageAge.doubleValue(), 0.1);
+		// 排序 空内容处理
+		List<User> sortAndLimitList = distList.stream().sorted(Comparator.nullsFirst(Comparator.comparing(User::getAge, this::compareInteger)).reversed()).collect(Collectors.toList());
+		sortAndLimitList.forEach(System.out::println);
 
-        IntSummaryStatistics sta = distList.stream().collect(Collectors.summarizingInt(User::getAge));
-        Assert.assertEquals(15, sta.getAverage(), 0.1);
-        Assert.assertEquals(20, sta.getMax());
-        Assert.assertEquals(3, sta.getCount());
-        Assert.assertEquals(45, sta.getSum());
+		// TO Map, Set, List (Collectors.toSet Or toList)
+		Map<String, User> userMap = distList.stream().collect(Collectors.toMap(User::getCode, u -> u));
+		List<String> codeList = distList.stream().map(User::getCode).collect(Collectors.toList());
+		userMap.forEach((k, v) -> System.out.println(k + ":" + v));
+		// To Map fix Duplicate key
+		Map<String, User> userMapDistinct = userList.stream().collect(Collectors.toMap(u -> u.getCode(), u -> u, (v1, v2) -> v2));
+		userMapDistinct.forEach((k, v) -> System.out.println(k + ":" + v));
 
-        // Join
-        String codes = distList.stream().map(User::getCode).collect(Collectors.joining("-"));
-        Assert.assertEquals("a-c-b", codes);
+		// Statistics: sum max age  and so on
+		int sumAge1 = distList.stream().collect(Collectors.reducing(0, User::getAge, (i, j) -> i + j));
+		int sumAge2 = distList.stream().collect(Collectors.reducing(0, User::getAge, Integer::sum));
+		int sumAge3 = distList.stream().mapToInt(User::getAge).sum();
+		int sumAge4 = distList.stream().collect(Collectors.summingInt(User::getAge));
+		Assert.assertEquals(45, sumAge1);
+		Assert.assertEquals(45, sumAge2);
+		Assert.assertEquals(45, sumAge3);
+		Assert.assertEquals(45, sumAge4);
 
-        // reduce TODO
-        //int cus = distList.stream().reduce(1, User::getAge, Integer::sum);
+		Optional<User> maxAgeUser = distList.stream().max(Comparator.comparingInt(User::getAge));
+		Assert.assertEquals("a", maxAgeUser.get().getCode());
 
-        // Group By TODO
-        distList.stream().collect(Collectors.groupingBy(User::getRoleId));
+		int maxAge1 = distList.stream().collect(Collectors.reducing(0, User::getAge, Integer::max));
+		int maxAge2 = distList.stream().mapToInt(User::getAge).max().orElse(-1);
+		Assert.assertEquals(20, maxAge1);
+		Assert.assertEquals(20, maxAge2);
 
-        // Filter
-        List<User> filterList = distList.stream().filter(d -> "a".equals(d.getCode())).map(d -> {
-            d.setName(d.getName() + "Change");
-            return d;
-        }).collect(Collectors.toList());
+		Double ageAge = distList.stream().collect(Collectors.averagingInt(User::getAge));
+		Assert.assertEquals(15, ageAge.doubleValue(), 0.1);
 
-        filterList.forEach(System.out::println);
+		IntSummaryStatistics sta = distList.stream().collect(Collectors.summarizingInt(User::getAge));
+		Assert.assertEquals(15, sta.getAverage(), 0.1);
+		Assert.assertEquals(20, sta.getMax());
+		Assert.assertEquals(3, sta.getCount());
+		Assert.assertEquals(45, sta.getSum());
 
-        // Parameter join
-        List<String> joinParamList = distList.stream().map(d -> d.getCode() + "-" + d.getName()).collect(Collectors.toList());
-        joinParamList.forEach(System.out::println);
+		// Join
+		String codes = distList.stream().map(User::getCode).collect(Collectors.joining("-"));
+		Assert.assertEquals("a-c-b", codes);
 
-        // findOne
-        boolean hasSomeOne = distList.stream().anyMatch(d -> "e".equals(d.getCode()));
-        Assert.assertFalse(hasSomeOne);
-    }
+		// reduce TODO
+		//int cus = distList.stream().reduce(1, User::getAge, Integer::sum);
 
-    @Test
-    public void testLongStreamToMap() {
+		// Group By TODO
+		distList.stream().collect(Collectors.groupingBy(User::getRoleId));
 
-        // 添加age重复内容
-        userList.add(User.builder().code("d").name("dName").age(15).roleId(2).build());
+		// Filter
+		List<User> filterList = distList.stream().filter(d -> "a".equals(d.getCode())).map(d -> {
+			d.setName(d.getName() + "Change");
+			return d;
+		}).collect(Collectors.toList());
+
+		filterList.forEach(System.out::println);
+
+		// Parameter join
+		List<String> joinParamList = distList.stream().map(d -> d.getCode() + "-" + d.getName()).collect(Collectors.toList());
+		joinParamList.forEach(System.out::println);
+
+		// findOne
+		boolean hasSomeOne = distList.stream().anyMatch(d -> "e".equals(d.getCode()));
+		Assert.assertFalse(hasSomeOne);
+	}
+
+	@Test
+	public void testLongStreamToMap() {
+
+		// 添加age重复内容
+		userList.add(User.builder().code("d").name("dName").age(15).roleId(2).build());
 //		userList.stream().mapToLong(User::getAge).distinct().collect(Collectors.toMap(a -> a, a -> a));
 
-        // 处理key重复内容（覆盖 or 拼接等自定义处理）
-        Map<Integer, String> map1 = userList.stream().collect(Collectors.toMap(User::getAge, User::getName, (val1, val2) -> val1 + "-" + val2));
-        System.out.println("map1:" + map1);
+		// 处理key重复内容（覆盖 or 拼接等自定义处理）
+		Map<Integer, String> map1 = userList.stream().collect(Collectors.toMap(User::getAge, User::getName, (val1, val2) -> val1 + "-" + val2));
+		System.out.println("map1:" + map1);
 
-        // 处理key重复为List
-        Map<Integer, List<String>> map2 = userList.stream().collect(Collectors.toMap(User::getAge, u -> Arrays.asList(u.getName()), (List<String> val1List, List<String> val2List) -> {
-                    System.out.println("val1:" + val1List);
-                    System.out.println("val2:" + val2List);
-                    val1List.addAll(val2List);
-                    return val1List;
-                }
-        ));
-        System.out.println("map2:" + map2);
-    }
+		// 处理key重复为List
+		Map<Integer, List<String>> map2 = userList.stream().collect(Collectors.toMap(User::getAge, u -> Arrays.asList(u.getName()), (List<String> val1List, List<String> val2List) -> {
+					System.out.println("val1:" + val1List);
+					System.out.println("val2:" + val2List);
+					val1List.addAll(val2List);
+					return val1List;
+				}
+		));
+		System.out.println("map2:" + map2);
+	}
 
 
-    @Test
-    public void testFlatMap() {
+	@Test
+	public void testFlatMap() {
 
-        Integer collect = userList.stream().collect(Collectors.reducing(0, User::getAge, (v1, v2) -> v1 + v2));
-        System.out.println(collect);
+		Integer collect = userList.stream().collect(Collectors.reducing(0, User::getAge, (v1, v2) -> v1 + v2));
+		System.out.println(collect);
 
-        Map<Integer, List<User>> collect1 = userList.stream().collect(Collectors.groupingBy(User::getRoleId, Collectors.toList()));
-        System.out.println(collect1);
+		Map<Integer, List<User>> collect1 = userList.stream().collect(Collectors.groupingBy(User::getRoleId, Collectors.toList()));
+		System.out.println(collect1);
 
 //        userList.stream().collect(Collectors.collectingAndThen(Collectors.groupingBy(User::getRoleId, Collectors.toList(), Collectors.toList())));
+	}
+
+	@Test
+	public void testMap() {
+
+		User user = null;
+		Map<String, Optional<User>> resultMap = userList.stream().collect(Collectors.toMap(User::getCode, a -> Optional.ofNullable(user)));
+		System.out.println(resultMap);
+	}
+
+	@Test
+	public void testGroupMap() {
+
+		// key roleId, value: List<name>
+		Map<Integer, List<String>> collect = userList.stream().collect(Collectors.groupingBy(User::getRoleId, Collectors.mapping(User::getName, Collectors.toList())));
+		System.out.println(collect);
 
 
-    }
+	}
 
-    // regionList.stream().filter(distinctByKey(t -> pickRegionTimezoneKey(t.getNationId(), t.getLevel1(), t.getLevel2(), t.getTimezone()))).collect(Collectors.toList());
-    // 按照某个属性进行distinct
-    private <T> Predicate<T> distinctByKey(Function<? super T, ?> keyExtractor) {
-        Map<Object, Boolean> seen = new ConcurrentHashMap<>();
-        return t -> seen.putIfAbsent(keyExtractor.apply(t), Boolean.TRUE) == null;
-    }
+	@Test
+	public void testOptional() {
+		User user = User.builder().code("a").name("aName").age(20).roleId(1).build();
+//        User user = null;
+		System.out.println(Optional.of(user).map(User::getCode).orElse("no"));
 
-    // 比较两个参数大小
-    private int compareInteger(Integer a1, Integer a2) {
-        int r = 0;
-        if (a1 == null) {
-            r = -1;
-        } else if (a2 == null) {
-            r = 1;
-        } else {
-            r = a1.compareTo(a2);
-        }
-        return r;
-    }
+
+	}
+
+	// regionList.stream().filter(distinctByKey(t -> pickRegionTimezoneKey(t.getNationId(), t.getLevel1(), t.getLevel2(), t.getTimezone()))).collect(Collectors.toList());
+
+	@Test
+	public void testDistinct() {
+		List<User> resultList = this.userList.stream().filter(distinctByKey(u -> u.getRoleId())).collect(Collectors.toList());
+		resultList.stream().forEach(System.out::println);
+	}
+
+
+	// 按照某个属性进行distinct
+	private <T> Predicate<T> distinctByKey(Function<? super T, ?> keyExtractor) {
+		Map<Object, Boolean> seen = new ConcurrentHashMap<>();
+		return t -> seen.putIfAbsent(keyExtractor.apply(t), Boolean.TRUE) == null;
+	}
+
+	// 比较两个参数大小
+	private int compareInteger(Integer a1, Integer a2) {
+		int r = 0;
+		if (a1 == null) {
+			r = -1;
+		} else if (a2 == null) {
+			r = 1;
+		} else {
+			r = a1.compareTo(a2);
+		}
+		return r;
+	}
 }
